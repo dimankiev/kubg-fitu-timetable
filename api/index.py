3bca340e32254003700c49851d779976e01a4ee0
Dmytro Nelipa <=>
2021-11-08 14:15:45 +0200

    Fixes

--- api/index.py
+++ api/index.py
@@ -1,11 +1,12 @@
 from http.server import BaseHTTPRequestHandler
 from ._getter import get_timetable_url
+import urllib.parse
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
         url = get_timetable_url()
-        self.send_header('Location',url)
+        self.send_header('Location', urllib.parse.quote_plus(url))
         self.end_headers()
         return
